ab50a1e00afb81b6e6895d5475b819025cc51bd2
Martin Wind <martin.wind@im-c.at>
2024-03-26 18:47:59 +0100

    add resize and logo

--- main.ts
+++ main.ts
@@ -1,19 +1,32 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const clock = new THREE.Clock();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const file = new URLSearchParams(window.location.search).get('img') || '1';
-const texture = new THREE.TextureLoader().load(`img/${file}.jpg`);
+const file =
+  new URLSearchParams(window.location.search).get("scene") || "default";
+const texture = new THREE.TextureLoader().load(`img/${file}.jpg`, () => {
+  document.body.removeChild(
+    document.getElementsByClassName("lds-hourglass")[0]
+  );
+});
 
-const geometry = new THREE.SphereGeometry(500, 60, 40)
-const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+const geometry = new THREE.SphereGeometry(500, 60, 40);
+const material = new THREE.MeshBasicMaterial({
+  map: texture,
+  side: THREE.DoubleSide,
+});
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
@@ -38,4 +51,25 @@ function animate() {
 }
 
 clock.start();
-requestAnimationFrame(animate)
+requestAnimationFrame(animate);
+
+addEventListener(
+  "resize",
+  () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  },
+  false
+);
+
+// Add logo in js to make it harder to remove
+const logo = document.createElement("img");
+logo.src = "img/logo.png";
+logo.style.position = "absolute";
+logo.style.bottom = "15px";
+logo.style.right = "15px";
+logo.style.width = "200px";
+logo.style.pointerEvents = "none";
+
+document.body.appendChild(logo);
