b363892ac7af631e6d7883ff3576debcef48b122
Martin Wind <martin.wind@im-c.at>
2024-03-30 09:35:16 +0100

    add cube map support

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# 360
+# 360.js
+
+360° Panorama viewer.
+
+Features:
+
+* Spherical panoramas ([example](dist/img/default.jpg))
+* Cube face panoramas ([example](dist/img/default/nx.jpg))
+* Configuration via URL params
+  * `scene` image source (default: `example`)
+  * `type` cube or sphere (default: `cube`)
+  * `speed` set the auto ration speed (default: `3`)
+
+## Building
+
+Run `yarn build` and copy your panoramas to `dist/img`.
+
+## Acknowledgment
+
+* [norikdavtian](https://norikdavtian.github.io/ThreeJS-360-Panorama/) ThreeJS-360-Panorama
+* [airmode](https://www.airmode.at/) example panorama
+* [jaxry](https://jaxry.github.io/panorama-to-cubemap/) panorama to cubemap converter
--- main.ts
+++ main.ts
@@ -10,29 +10,27 @@ const camera = new THREE.PerspectiveCamera(
 );
 const clock = new THREE.Clock();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-
+const manager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader(manager);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const file =
-  new URLSearchParams(window.location.search).get("scene") || "default";
-const texture = new THREE.TextureLoader().load(`img/${file}.jpg`, () => {
-  document.body.removeChild(
-    document.getElementsByClassName("lds-hourglass")[0]
-  );
-});
-const geometry = new THREE.SphereGeometry(500, 64, 32);
-const material = new THREE.MeshBasicMaterial({
-  map: texture,
-  side: THREE.BackSide, // Texture is on the inside
-});
-const sphere = new THREE.Mesh(geometry, material);
+const urlParams = new URLSearchParams(window.location.search);
+const sceneType =
+  (urlParams.get("type") || "cube").toLowerCase() === "cube"
+    ? "cube"
+    : "sphere";
+const source = `img/${urlParams.get("scene") || "example"}`;
+const autoRotationSpeed = parseInt(urlParams.get("speed") || "3") / 100;
 
-scene.add(sphere);
-camera.position.set(0, 0, 0);
-sphere.position.set(0, 0, 0);
+if (sceneType === "sphere") {
+  scene.add(createPanoramaSphere(source));
+} else {
+  scene.add(createPanoramaCube(source));
+}
 
 const controls = new PanoramaControls(camera, renderer.domElement);
+controls.autoRotationSpeed = autoRotationSpeed;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -43,6 +41,13 @@ function animate() {
 clock.start();
 requestAnimationFrame(animate);
 
+manager.onLoad = () => {
+  const loadingEls = document.getElementsByClassName("loading");
+  for (let i = 0; i < loadingEls.length; i++) {
+    document.body.removeChild(loadingEls[i]);
+  }
+};
+
 addEventListener(
   "resize",
   () => {
@@ -52,3 +57,64 @@ addEventListener(
   },
   false
 );
+
+function createPanoramaSphere(url: string) {
+  const file =
+    new URLSearchParams(window.location.search).get("scene") || "default";
+  const texture = textureLoader.load(`${url}.jpg`);
+  const geometry = new THREE.SphereGeometry(500, 64, 32);
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.BackSide, // Texture is on the inside
+  });
+  const sphere = new THREE.Mesh(geometry, material);
+  sphere.scale.x = -1; // Invert the sphere to fix the texture orientation
+  return sphere;
+}
+
+function createPanoramaCubePlane(url: string) {
+  const texture = textureLoader.load(url);
+  const geometry = new THREE.PlaneGeometry(400, 400);
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+  });
+  return new THREE.Mesh(geometry, material);
+}
+
+/**
+ * Cube faces can be generated with https://jaxry.github.io/panorama-to-cubemap/
+ */
+function createPanoramaCube(url: string) {
+  const cube = new THREE.Object3D();
+
+  const px = createPanoramaCubePlane(`${url}/px.jpg`);
+  cube.add(px);
+  px.position.set(200, 0, 0);
+  px.rotateY(-Math.PI / 2);
+
+  const py = createPanoramaCubePlane(`${url}/py.jpg`);
+  cube.add(py);
+  py.position.set(0, 200, 0);
+  py.rotateX(Math.PI / 2);
+
+  const pz = createPanoramaCubePlane(`${url}/pz.jpg`);
+  cube.add(pz);
+  pz.position.set(0, 0, -200);
+
+  const nx = createPanoramaCubePlane(`${url}/nx.jpg`);
+  cube.add(nx);
+  nx.position.set(-200, 0, 0);
+  nx.rotateY(Math.PI / 2);
+
+  const ny = createPanoramaCubePlane(`${url}/ny.jpg`);
+  cube.add(ny);
+  ny.position.set(0, -200, 0);
+  ny.rotateX(-Math.PI / 2);
+
+  const nz = createPanoramaCubePlane(`${url}/nz.jpg`);
+  cube.add(nz);
+  nz.position.set(0, 0, 200);
+  nz.rotateY(Math.PI);
+
+  return cube;
+}
